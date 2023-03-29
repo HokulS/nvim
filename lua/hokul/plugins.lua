e993f8e39ba5675ff283fa473b967f46792317b3
Hokul Srikanthan <h.srikanthan@uqconnect.edu.au>
2023-03-29 18:14:36 +1000

    Added comment plugin

--- lua/hokul/plugins.lua
+++ lua/hokul/plugins.lua
@@ -63,6 +63,8 @@ return packer.startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
 
+  use 'numToStr/Comment.nvim'
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
